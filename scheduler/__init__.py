2241b1726dcbfda17fab1fad7058bf034d6f2545
sssl93 <liangbing@suomitek.com>
2018-12-21 18:13:03 +0800

    任务调度器初始化

---
+++ scheduler/__init__.py
@@ -0,0 +1,15 @@
+class Scheduler:
+    def __init__(self):
+        self.name = None
+
+    def send_task(self, task_id, params):
+        pass
+
+    def execute_task(self, task_id, params):
+        pass
+
+    def handle_result(self):
+        pass
+
+    def run_forever(self):
+        pass
